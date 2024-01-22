3b7abce73e720354186903463a77332f05c40cf6
suman7802 <mansu7802@gmail.com>
2024-01-22 15:22:26 +0545

    get list of songs (ACC)

--- server/src/controllers/download.controller.js
+++ server/src/controllers/download.controller.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 import ytdl from 'ytdl-core';
+import archiver from 'archiver';
 import {google} from 'googleapis';
 
 import catchAsync from '../errors/catchAsync.js';
@@ -11,6 +12,41 @@ dotenv.config();
 
 const {API_KEY} = process.env;
 const youtube = google.youtube('v3');
+
+async function getPlaylistItems(playlistId, pageToken = null) {
+  const response = await youtube.playlistItems.list({
+    auth: API_KEY,
+    part: 'snippet',
+    playlistId: playlistId,
+    maxResults: 50,
+    pageToken: pageToken,
+  });
+  return response.data;
+}
+
+async function getAllPlaylistItems(playlistId) {
+  let allItems = [];
+  let pageToken = null;
+  do {
+    const response = await getPlaylistItems(playlistId, pageToken);
+    const items = response.items;
+    if (items) {
+      allItems = allItems.concat(items);
+    }
+    pageToken = response.nextPageToken;
+  } while (pageToken);
+  return allItems;
+}
+
+async function getVideoUrls(playlistId) {
+  const playlistItems = await getAllPlaylistItems(playlistId);
+
+  const videoUrls = playlistItems.map((item) => {
+    const videoId = item.snippet.resourceId.videoId;
+    return `https://www.youtube.com/watch?v=${videoId}`;
+  });
+  return videoUrls;
+}
 
 const download = {
   singleAccFormat: catchAsync(async (req, res) => {
@@ -51,43 +87,51 @@ const download = {
 
     const playlistId = new URLSearchParams(new URL(url).search).get('list');
 
-    async function getPlaylistItems(playlistId, pageToken = null) {
-      const response = await youtube.playlistItems.list({
-        auth: API_KEY,
-        part: 'snippet',
-        playlistId: playlistId,
-        maxResults: 50,
-        pageToken: pageToken,
+    const videoUrls = await getVideoUrls(playlistId);
+
+    const dateAndTime = new Date().toISOString();
+
+    const dir = `../downloads/playlist${dateAndTime}`;
+
+    const filePaths = [];
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {recursive: true});
+    }
+
+    const archive = archiver('zip');
+    res.attachment('songs.zip');
+    archive.pipe(res);
+
+    const promises = videoUrls.map(async (videoUrl) => {
+      const video = ytdl(videoUrl, {
+        filter: 'audioonly',
+        quality: 'highestaudio',
       });
-      return response.data;
-    }
 
-    async function getAllPlaylistItems(playlistId) {
-      let allItems = [];
-      let pageToken = null;
-      do {
-        const response = await getPlaylistItems(playlistId, pageToken);
-        const items = response.items;
-        if (items) {
-          allItems = allItems.concat(items);
-        }
-        pageToken = response.nextPageToken;
-      } while (pageToken);
-      return allItems;
-    }
+      const title = (await ytdl.getInfo(videoUrl)).videoDetails.title.replace(
+        /[^\w\s]/gi,
+        ''
+      );
 
-    async function getVideoUrls(playlistId) {
-      const playlistItems = await getAllPlaylistItems(playlistId);
+      const filePath = path.join(dir, `${title}.aac`);
+      filePaths.push(filePath);
 
-      const videoUrls = playlistItems.map((item) => {
-        const videoId = item.snippet.resourceId.videoId;
-        return `https://www.youtube.com/watch?v=${videoId}`;
+      const writeStream = fs.createWriteStream(filePath);
+      video.pipe(writeStream);
+
+      return new Promise((resolve) => {
+        writeStream.on('finish', () => {
+          console.log(`Downloaded ${title}`);
+          archive.file(filePath, {name: path.basename(filePath)});
+          resolve();
+        });
       });
-      return videoUrls;
-    }
+    });
 
-    const result = await getVideoUrls(playlistId);
-    console.log(result); // it provide all the video urls in the playlist
+    await Promise.all(promises);
+
+    await archive.finalize();
   }),
 };
 
