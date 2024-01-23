08c4c8d61e7616c2311987a95c88922e9352793e
suman7802 <mansu7802@gmail.com>
2024-01-23 16:09:37 +0545

    added static file

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
 import cors from 'cors';
+import path from 'path';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import {dirname} from 'path';
 import express from 'express';
+import {fileURLToPath} from 'url';
 
 import downloadRouter from './routes/download.route.js';
 import errorHandler from './controllers/errorHandler.js';
@@ -10,6 +13,9 @@ import errorHandler from './controllers/errorHandler.js';
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const isLocal = process.env.NODE_ENV === 'development';
 
@@ -25,6 +31,11 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/download', downloadRouter);
 app.use(errorHandler);
+
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on  http://localhost:${PORT}`);
